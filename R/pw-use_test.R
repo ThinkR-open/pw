102384610bb81a2640aa198d6173a9fbc6441bf5
colin <colin@thinkr.fr>
2025-09-19 11:22:45 +0200

    feat: allow to create a test file

---
+++ R/pw-use_test.R
@@ -0,0 +1,37 @@
+#' Show the Playwright report
+#'
+#' @param name The name of the test to add (without .test.ts extension)
+#' @param where The path to the package
+#' @importFrom fs file_copy
+#' @importFrom withr with_dir
+#' @export
+pw_use_test <- function(
+  name,
+  where = golem::get_golem_wd()
+) {
+  if (missing(name) || !is.character(name) || length(name) != 1) {
+    stop("Please provide a single test name as a string.")
+  }
+  stop_if_npx_not_available()
+  stop_if_playwright_skeleton_not_present(
+    where = where
+  )
+  with_dir(
+    where,
+    {
+      with_dir("tests", {
+        with_dir("playwright", {
+          file_copy(
+            pw_sys_files(
+              "default.test.ts"
+            ),
+            sprintf(
+              "%s.test.ts",
+              name
+            )
+          )
+        })
+      })
+    }
+  )
+}
---
+++ tests/testthat/test-pw-use_test.R
@@ -0,0 +1,41 @@
+test_that("pw_use_test works", {
+  temp_dir <- fs::path_temp(
+    "pwtest"
+  )
+  unlink(
+    temp_dir,
+    recursive = TRUE
+  )
+  dir.create(
+    file.path(
+      temp_dir,
+      "tests",
+      "playwright"
+    ),
+    recursive = TRUE
+  )
+  testthat::with_mocked_bindings(
+    stop_if_npx_not_available = function(...) NULL,
+    stop_if_playwright_skeleton_not_present = function(...) NULL,
+    {
+      pw_use_test(
+        name = "my_test",
+        where = temp_dir
+      )
+    }
+  )
+  expect_true(
+    file.exists(
+      fs::path(
+        temp_dir,
+        "tests",
+        "playwright",
+        "my_test.test.ts"
+      )
+    )
+  )
+  unlink(
+    temp_dir,
+    recursive = TRUE
+  )
+})
